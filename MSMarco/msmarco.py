d80e9e94d4ce8bce6d897b6ee66ae7ca0b132e28
Jack Pertschuk <jp954@cornell.edu>
2019-12-05 13:57:25 -0500

    .

--- MSMarco/msmarco.py
+++ MSMarco/msmarco.py
@@ -6,6 +6,7 @@ from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler,
                               TensorDataset)
 import os
 import pathlib
+from collections import defaultdict
 
 
 class InputFeatures(object):
@@ -145,6 +146,21 @@ def train():
       tokenizer.save_pretrained(args.save_dir)
   model.save_pretrained(args.save_dir)
   tokenizer.save_pretrained(args.save_dir)
+
+
+def eval():
+  qrels = []
+  with open('./qrels.dev.small', 'r') as qrels_file:
+    for line in qrels_file:
+      qid, cid = line.rstrip().split('\t')
+      qrels.append((qid, cid))
+
+  dev_set = defaultdict(list)
+  with open('./top1000.dev', 'r') as dev_file:
+    for line in dev_file:
+      qid, cid, query, candidate = line.rstrip().split('\t')
+      label = 1 if (qid, cid) in qrels else 0
+      dev_set[query].append((candidate, label))
 
 
 if __name__ == '__main__':
