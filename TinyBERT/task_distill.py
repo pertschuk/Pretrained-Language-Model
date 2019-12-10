d3918af9e0c8febdfca1d006857b1a1ef71e8de2
Jack Pertschuk <jp954@cornell.edu>
2019-12-10 18:23:46 -0500

    .

--- TinyBERT/task_distill.py
+++ TinyBERT/task_distill.py
@@ -626,6 +626,8 @@ def compute_metrics(task_name, preds, labels):
     return {"acc": simple_accuracy(preds, labels)}
   elif task_name == "wnli":
     return {"acc": simple_accuracy(preds, labels)}
+  elif task_name == "msmarco":
+    return {"acc": simple_accuracy(preds, labels)}
   else:
     raise KeyError(task_name)
 
