09b89afde6f7afda152a5f0d01925ff7ac21b49b
Jack Pertschuk <jp954@cornell.edu>
2019-12-10 20:53:42 -0500

    .

--- TinyBERT/transformer/modeling.py
+++ TinyBERT/transformer/modeling.py
@@ -46,6 +46,7 @@ PRETRAINED_MODEL_ARCHIVE_MAP = {
     'bert-large-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-cased.tar.gz",
     'bert-base-multilingual-uncased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-uncased.tar.gz",
     'bert-base-multilingual-cased': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-multilingual-cased.tar.gz",
+    'bert-base-cased-finetuned-mrpc': "https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-cased-finetuned-mrpc.tar.gz",
     'bert-base-chinese': "",
 }
 
