47b789483e11ed1008776fb0cba9113809bd4475
Jack Pertschuk <jp954@cornell.edu>
2019-12-11 23:53:15 -0500

    .

---
+++ QA/qa_model.py
@@ -0,0 +1,71 @@
+from transformers import *
+import torch
+import numpy as np
+
+MODEL = 'distilbert-base-uncased-distilled-squad'
+max_query_length = 64
+max_seq_length = 512
+
+
+def _is_whitespace(c):
+  if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+    return True
+  return False
+
+def run_squad(question, context):
+  model = DistilBertForQuestionAnswering.from_pretrained(MODEL)
+  tokenizer = DistilBertTokenizer.from_pretrained(MODEL)
+
+  doc_tokens = []
+  char_to_word_offset = []
+  prev_is_whitespace = True
+
+  # Split on whitespace so that different tokens may be attributed to their original position.
+  for c in context:
+    if _is_whitespace(c):
+      prev_is_whitespace = True
+    else:
+      if prev_is_whitespace:
+        doc_tokens.append(c)
+      else:
+        doc_tokens[-1] += c
+      prev_is_whitespace = False
+    char_to_word_offset.append(len(doc_tokens) - 1)
+
+  tok_to_orig_index = []
+  orig_to_tok_index = []
+  all_doc_tokens = []
+  for (i, token) in enumerate(doc_tokens):
+    orig_to_tok_index.append(len(all_doc_tokens))
+    sub_tokens = tokenizer.tokenize(token)
+    for sub_token in sub_tokens:
+      tok_to_orig_index.append(i)
+      all_doc_tokens.append(sub_token)
+
+  spans = []
+
+  truncated_query = tokenizer.encode(question, add_special_tokens=False, max_length=max_query_length)
+  sequence_added_tokens = tokenizer.max_len - tokenizer.max_len_single_sentence
+  sequence_pair_added_tokens = tokenizer.max_len - tokenizer.max_len_sentences_pair
+
+  encoded_dict = tokenizer.encode_plus(
+    truncated_query if tokenizer.padding_side == "right" else all_doc_tokens,
+    all_doc_tokens if tokenizer.padding_side == "right" else truncated_query,
+    max_length=max_seq_length,
+    return_overflowing_tokens=True,
+    truncation_strategy='only_second' if tokenizer.padding_side == "right" else 'only_first',
+    return_tensors='pt'
+  )
+
+  model.eval()
+  with torch.no_grad():
+    start_logits, end_logits = model(input_ids=encoded_dict['input_ids'],
+                    token_type_ids=encoded_dict['token_type_ids'])
+  start_tok = int(np.argmax(start_logits))
+  end_tok = int(np.argmax(end_logits[start_tok:])) + start_tok
+  return ' '.join(doc_tokens[tok_to_orig_index[start_tok]:tok_to_orig_index[end_tok]])
+
+
+if __name__ == '__main__':
+  test_squad()
+
--- TinyBERT/distill.sh
+++ TinyBERT/distill.sh
@@ -3,10 +3,10 @@
 export FT_BERT_BASE_DIR=./pt-bert-base-uncased-msmarco/
 export GENERAL_TINYBERT_DIR=./2nd_General_TinyBERT_4L_312D/
 export TASK_DIR=./data
-export TMP_TINYBERT_DIR=./tinybert-msmarco
+export TMP_TINYBERT_DIR=./tinybert-msmarco-10
 export TASK_NAME=msmarco
 
-mkdir ./tinybert-msmarco
+mkdir $TMP_TINYBERT_DIR
 python3 task_distill.py --teacher_model ${FT_BERT_BASE_DIR} \
                        --student_model ${GENERAL_TINYBERT_DIR} \
                        --data_dir ${TASK_DIR} \
@@ -18,7 +18,7 @@ python3 task_distill.py --teacher_model ${FT_BERT_BASE_DIR} \
                        --do_lower_case
 
 #export TMP_TINYBERT_DIR=./tinybert-msmarco-ft
-export TINYBERT_DIR=./tinybert-msmarco-ft-1
+export TINYBERT_DIR=./tinybert-msmarco-ft
 mkdir TINYBERT_DIR
 python3 task_distill.py --pred_distill  \
                        --teacher_model ${FT_BERT_BASE_DIR} \
