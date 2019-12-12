985044a18ccde97e3547b427d29d448d531a9170
Jack Pertschuk <jp954@cornell.edu>
2019-12-12 14:33:33 -0500

    .

--- QA/qa_model.py
+++ QA/qa_model.py
@@ -58,10 +58,10 @@ def run_squad(question, context):
   model.eval()
   with torch.no_grad():
     start_logits, end_logits = model(input_ids=encoded_dict['input_ids'])
-  import pdb
-  pdb.set_trace()
-  start_tok = int(np.argmax(start_logits))
-  end_tok = int(np.argmax(end_logits[start_tok:])) + start_tok
+  # import pdb
+  # pdb.set_trace()
+  start_tok = int(np.argmax(start_logits[0]))
+  end_tok = int(np.argmax(end_logits[0][start_tok:])) + start_tok
   return ' '.join(doc_tokens[tok_to_orig_index[start_tok]:tok_to_orig_index[end_tok]])
 
 
